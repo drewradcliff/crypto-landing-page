dfdd7c8e0a0a7c8575735dd9320a6341d40890f8
Drew Radcliff <sirampr@gmail.com>
2022-01-08 13:39:40 -0500

    Add header

---
+++ components/Dropdown.tsx
@@ -0,0 +1,79 @@
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import {
+  PencilIcon,
+  MenuIcon,
+  PresentationChartLineIcon,
+} from "@heroicons/react/outline";
+
+export default function Dropdown() {
+  return (
+    <div className="md:hidden">
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex justify-center w-full px-4 py-2">
+            <MenuIcon className="h-5 w-5" />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="px-1 py-1 ">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? "bg-violet-500 text-white" : "text-gray-900"
+                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                  >
+                    Market
+                  </button>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? "bg-violet-500 text-white" : "text-gray-900"
+                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                  >
+                    Activity
+                  </button>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? "bg-violet-500 text-white" : "text-gray-900"
+                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                  >
+                    Community
+                  </button>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? "bg-violet-500 text-white" : "text-gray-900"
+                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                  >
+                    Connect Wallet
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Dropdown from "./Dropdown";
+
+export default function Header() {
+  return (
+    <header className="container mx-auto flex p-5 items-center justify-between">
+      <a className="text-4xl font-bold">nft.io</a>
+      <nav className="ml-auto mr-auto md:flex hidden text-md text-gray-800">
+        <a className="mr-5">Market</a>
+        <a className="mr-5">Activity</a>
+        <a className="mr-5">Community</a>
+        <a>Connect Wallet</a>
+      </nav>
+      <Dropdown />
+    </header>
+  );
+}
